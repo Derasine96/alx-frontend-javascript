95682dfc0fde0eea9cd4146b6f8804cc0a57d4db
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-04-23 18:56:55 +0000

    Promise settle

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return (Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results)
-  );
+  const signupPromise = signUpUser(firstName, lastName)
+    .then((data) => ({ status: 'fulfilled', value: data }))
+    .catch((error) => ({ status: 'rejected', value: error }));
+
+  const photoPromise = uploadPhoto(fileName)
+    .then((data) => ({ status: 'fulfilled', value: data }))
+    .catch((error) => ({ status: 'rejected', value: error }));
+
+  return Promise.all([signupPromise, photoPromise]);
 }
