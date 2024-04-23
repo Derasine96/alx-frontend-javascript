54915172f745cf5732512d3079a0dbb511ca0a46
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-04-23 18:58:55 +0000

    Promise settle

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signupPromise = signUpUser(firstName, lastName)
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const signup = await signUpUser(firstName, lastName)
     .then((data) => ({ status: 'fulfilled', value: data }))
-    .catch((error) => ({ status: 'rejected', value: error }));
-
-  const photoPromise = uploadPhoto(fileName)
+    .catch((err) => ({ status: 'rejected', value: err }));
+  const photo = await uploadPhoto(fileName)
     .then((data) => ({ status: 'fulfilled', value: data }))
-    .catch((error) => ({ status: 'rejected', value: error }));
+    .catch((err) => ({ status: 'rejected', value: err.toString() }));
 
-  return Promise.all([signupPromise, photoPromise]);
+  return [signup, photo];
 }
