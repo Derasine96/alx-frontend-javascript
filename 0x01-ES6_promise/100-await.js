0033ea94ac18ea3383fb0e9098dafbe3617d3b41
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-04-24 21:18:28 +0000

    Aync and await function

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    console.error('Error:', error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
