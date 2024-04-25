586de8828091b98ab0eb85bb9b18dbd95cacb0e9
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-04-25 16:12:21 +0000

    Fix type check

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,12 +8,12 @@ export default class HolbertonCourse {
       throw new TypeError('Length must be a number');
     }
 
-    if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array');
+    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
     }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
