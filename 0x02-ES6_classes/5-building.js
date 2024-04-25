9f859d281d64fd4fcca56e38eb8bdca6958e20da
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-04-25 17:33:59 +0000

    Abstract class

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["evacuationWarningMessage"] }] */
+
+export default class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new TypeError('Cannot instantiate abstract class');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
