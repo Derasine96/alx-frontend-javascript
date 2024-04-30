277436040caf965d6839da10b0e2d3479208600f
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-04-30 10:14:14 +0000

    ES6 Data Manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  if (!(set instanceof Set) || !startString || typeof startString !== 'string') {
+    return '';
+  }
+
+  const filteredSet = new Set([...set].filter((value) => value.startsWith(startString)));
+
+  const cleanedElements = Array.from(filteredSet).map((value) => value.slice(startString.length));
+  return cleanedElements.join('-');
+}
